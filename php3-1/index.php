a88c4a741c79900e10e40db35d17263ac45e2795
anciuvis <nonofthem@gmail.com>
2018-01-29 10:08:23 +0200

    new ToDoList

--- php3-1/index.php
+++ php3-1/index.php
@@ -1,40 +1,39 @@
 <?php
-
 	$date = mktime(0,0,0,1,29,2000);
 	echo $date.'<br />';
-
 	$newDate = date('y/m/D', $date);
-
 	echo $newDate.'<br /><br />';
-
+	// pradinis masyvas kuri reik isrusiuot
 	$mas = [
-		['name'=> 'Petras', 'age'=> 24],
+		['name'=> 'Petras', 'age'=> 26],
+		// ['name'=> 'Petras', 'age'=> 25],
 		['name'=> 'Alius', 'age'=> 35],
 		['name'=> 'Zana', 'age'=> 21]
 	];
-
 	// foreach ($mas as $value){
 	// 	echo $value['name'].'<br />';
 	// }
-
+	// susikuriam raktu masyvus, akd juos paduoti, kad pagal juos eitu rusiavimas
 	$name = [];
 	foreach ($mas as $value) {
 		$name[]=$value['name'];
 	}
-
 	$age = [];
 	foreach ($mas as $value) {
 		$age[]=$value['age'];
 	}
-
 	// foreach ($name as $value) {
 	// 	echo $value.'<br>';
 	// }
-
+	echo 'rusiuojam pagal amziu:<br>';
 	array_multisort($age, SORT_ASC, $mas); // jeigu keisti pirma argumenta - pagal ji rusiuoja (amzius arba vardas siuo atveju)
-
 	foreach ($mas as $value) {
 		echo $value['name'].'-'.$value['age'].'<br>';
 	}
-
+	echo '<br />';
+	echo 'rusiuojam pagal varda:<br>';
+	array_multisort($name, SORT_ASC, $mas);
+	foreach ($mas as $value) {
+		echo $value['name'].'-'.$value['age'].'<br>';
+	}
  ?>
---
+++ php3-2/index.view.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>ToDoList PHP</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+	</head>
+	<body>
+		<div class="container">
+			<?php if (): ?>
+
+			<?php else: ?>
+				<?php if (): ?>
+
+				<?php else: ?>
+
+					<?php
+
+					?>
+
+				<?php endif;?>
+			<?php endif;?>
+		</div>
+		<script src="http://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+	</body>
+</html>
